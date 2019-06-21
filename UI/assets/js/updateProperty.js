8575135dad78eeea15d2c05efe0bea836ddedeff
Jayne-darl <janeuchechukwu@gmail.com>
2019-06-21 18:26:28 +0100

    feat(update-property): agent can update the details of a property
    [delivers: #166720761]

---
+++ UI/assets/js/updateProperty.js
@@ -0,0 +1,7 @@
+const submitButton = document.querySelector("#updateProperty");
+
+submitButton.addEventListener("click", submit);
+
+function submit() {
+  document.location.href = "./agent-dashboard.html";
+}
