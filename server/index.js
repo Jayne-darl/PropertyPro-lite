fbe86041bb1d77e59205a1c4eb0e0cabb7714056
Jayne-darl <janeuchechukwu@gmail.com>
2019-06-24 12:11:58 +0100

    ch(setup-environment): set up the environment for developint the api endpoints
    [delivers #166869380]

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+// app.use('/api/v1');
+
+app.get('/', (req, res) => res.status(200).json({ message: 'YAY! Congratulations! My first endpoint is working' }));
+
+app.all('*', (req, res) => {
+  res
+    .status(404)
+    .json({ message: 'Sorry, such endpoint does not exist' });
+});
+
+app.listen(3000);
+// console.log('app running on port ', 3000);
