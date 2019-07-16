7ac66864c8fb502d69a81b40aef2b353f10d2684
Jayne-darl <janeuchechukwu@gmail.com>
2019-07-16 18:28:22 +0100

    bg(refactor-test-suite):refator test suite
    [delivers #167326151]

---
+++ test/users.spec.js
@@ -0,0 +1,245 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import request from 'supertest';
+import Seed from '../server/model/seed';
+import db from '../server/model/dbConnection';
+import app from '../server/index';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+before(async () => {
+  try {
+    await db.query(
+      'TRUNCATE users CASCADE; ALTER SEQUENCE users_id_seq RESTART WITH 1;',
+    );
+    await db.query(
+      'TRUNCATE property; ALTER SEQUENCE property_id_seq RESTART WITH 1;',
+    );
+  } catch (error) {
+    // console.log(error);
+  }
+});
+
+const signup = '/api/v1/auth/create';
+// let token;
+
+describe('POST /api/v1/auth/create', () => {
+  it('should create a new user account', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.account)
+      .end((err, res) => {
+        // token = res.body.token;
+        expect(res.status).to.be.equal(201);
+        expect(res).to.have.status('201');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        done();
+      });
+  });
+
+  it('should return an error if a user does not supply the first name', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.emptyFirstName)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You need to supply a valid first name');
+        done();
+      });
+  });
+  it('should return an error if a user does not supply the last name', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.emptyLastName)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You need to supply a valid last name');
+        done();
+      });
+  });
+
+  it('should return an error if the email supplied is invalid', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.invalidEmail)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Enter a valid email');
+        done();
+      });
+  });
+
+  it('should return an error if the password format supplied is wrong', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.inValidPassword)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You need to supply password of minimum length of 8 characters');
+        done();
+      });
+  });
+
+  it('should return an error if the phone number format supplied is wrong', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.inValidNumber)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You need to supply a valid phone number');
+        done();
+      });
+  });
+  it('should return an error if no address is supplied', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.emptyAddressField)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You need to supply an address');
+        done();
+      });
+  });
+
+  it('should return an error if the email has been registered before', (done) => {
+    request(app)
+      .post(signup)
+      .set('Accept', 'application/json')
+      .send(Seed.account)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(403);
+        expect(res).to.have.status('403');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('error');
+        expect(res.body.error).to.be.equal('Action Forbidden. User already exist');
+        done();
+      });
+  });
+});
+
+const login = '/api/v1/auth/login';
+
+describe('POST /api/v1/auth/login ', () => {
+  it('should login a user', (done) => {
+    request(app)
+      .post(login)
+      .set('Accept', 'application/json')
+      .send(Seed.logIn)
+      .end((err, res) => {
+        console.log(res.body.data.token);
+        expect(res.status).to.be.equal(200);
+        expect(res).to.have.status('200');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        expect(res.body.data).to.include.key('token');
+        expect(res.body.data).to.include.key('id');
+        expect(res.body.data).to.include.key('email');
+        expect(res.body.data).to.include.key('first_name');
+        expect(res.body.data).to.include.key('last_name');
+        expect(res.body.data).to.include.key('password');
+        expect(res.body.data).to.include.key('phone_number');
+        expect(res.body.data).to.include.key('address');
+        expect(res.body.data).to.include.key('is_admin');
+        expect(res.body.data).to.include.key('registered');
+        done();
+      });
+  });
+  it("should return an error if password isn't supplied", (done) => {
+    request(app)
+      .post(login)
+      .set('Accept', 'application/json')
+      .send(Seed.emptyPassword)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You need to provide a password');
+        done();
+      });
+  });
+  it("should return an error if email isn't supplied", (done) => {
+    request(app)
+      .post(login)
+      .set('Accept', 'application/json')
+      .send(Seed.emptyEmail)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Ensure you provided an email and it is correct');
+        done();
+      });
+  });
+  it('should return an error if an invalid email is supplied', (done) => {
+    request(app)
+      .post(login)
+      .set('Accept', 'application/json')
+      .send(Seed.wrongPassword)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(422);
+        expect(res).to.have.status('422');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('The password you provided is incorrect');
+        done();
+      });
+  });
+  it('should return an error if an invalid password is supplied', (done) => {
+    request(app)
+      .post(login)
+      .set('Accept', 'application/json')
+      .send(Seed.invalidPassword)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Password length must be 8 characters and above');
+        done();
+      });
+  });
+  it('should return an error for a non existing account', (done) => {
+    request(app)
+      .post(login)
+      .set('Accept', 'application/json')
+      .send(Seed.nonExistingAccount)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res).to.have.status('404');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('User not found');
+        done();
+      });
+  });
+});
---
+++ test/zproperty.spec.js
@@ -0,0 +1,491 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import request from 'supertest';
+import path from 'path';
+import app from '../server/index';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+// eslint-disable-next-line prefer-destructuring
+
+const { token } = process.env;
+describe('POST /api/v1/property', () => {
+  it('should post a new property', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .attach('image_url', path.join(`${__dirname}/testImage/barn.jpg`))
+      .end((err, res) => {
+        expect(res.status).to.be.equal(201);
+        expect(res).to.have.status('201');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        done();
+      });
+  });
+  it('should  not post a new property with wrong image file', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .attach('image_url', path.join(`${__dirname}/testImage/localFile.zip`))
+      .end((err, res) => {
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        done();
+      });
+  });
+  it('should return an error if invalid token is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'ksehfdslfiwefhbjhsvdkcjhshj')
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(403);
+        expect(res).to.have.status('403');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('error');
+        done();
+      });
+  });
+  it('should return an error if no token is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        done();
+      });
+  });
+  it('should return an error if no price is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: '',
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Ensure price field is filled and with numeric characters');
+        done();
+      });
+  });
+  it('should return an error if no price is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: 20000,
+        state: '',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Ensure state field  is filled and with filled with alphabetical characters');
+        done();
+      });
+  });
+  it('should return an error if no price is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'aba',
+        city: '',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Ensure city field and with filled with alphabetical characters');
+        done();
+      });
+  });
+  it('should return an error if no price is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'aba',
+        city: 'lugbe',
+        address: '',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Ensure address field and with filled with alphabetical characters');
+        done();
+      });
+  });
+  it('should return an error if no price is provided', (done) => {
+    request(app)
+      .post('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'aba',
+        city: 'lugbe',
+        address: '2 akerele street',
+        type: '',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Ensure type field is not empty');
+        done();
+      });
+  });
+});
+
+describe('PATCH /api/v1/property/:property_id', () => {
+  it('should update property details', (done) => {
+    request(app)
+      .patch('/api/v1/property/1')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        // state: 'Abuja',
+        // city: 'Lugbe',
+        // address: '4 Federal Housing',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        // expect(res.body).to.include.key('token');
+        done();
+      });
+  });
+  it('should return an error for invalid id type', (done) => {
+    request(app)
+      .patch('/api/v1/property/y')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        type: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+      })
+      .attach('image_url', path.join(`${__dirname}/testImage/barn.jpg`))
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Id must be a number');
+        done();
+      });
+  });
+  it('should return an error if property is non existent', (done) => {
+    request(app)
+      .patch('/api/v1/property/1200')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .field({
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+      })
+      .attach('image_url', path.join(`${__dirname}/testImage/barn.jpg`))
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res).to.have.status('404');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You have not created any advert with this id');
+        done();
+      });
+  });
+  it('should return an error if no token is provided', (done) => {
+    request(app)
+      .patch('/api/v1/property/1')
+      .set('Accept', 'application/json')
+      .field({
+        status: 'available',
+        price: 20000,
+        state: 'Abuja',
+        city: 'Lugbe',
+        address: '4 Federal Housing',
+        type: '3 Bedroom',
+      })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        done();
+      });
+  });
+});
+
+describe('PATCH /api/v1/property/:property_id/sold', () => {
+  it('should return an error if no token is provided', (done) => {
+    request(app)
+      .patch('/api/v1/property/1/sold')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        done();
+      });
+  });
+  it('should mark a property as sold', (done) => {
+    request(app)
+      .patch('/api/v1/property/1/sold')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res).to.have.status('200');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        expect(res.body.data.status).to.be.equal('Sold');
+        done();
+      });
+  });
+  it('should return an error for invalid id type', (done) => {
+    request(app)
+      .patch('/api/v1/property/y/sold')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Id must be a number');
+        done();
+      });
+  });
+  it('should return an error for non existent property', (done) => {
+    request(app)
+      .patch('/api/v1/property/1200/sold')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res).to.have.status('404');
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('You are not authorize to mark this advert as sold');
+        done();
+      });
+  });
+});
+
+describe('GET /api/v1/property', () => {
+  it('should get all posted property adverts', (done) => {
+    request(app)
+      .get('/api/v1/property')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res).to.have.status('200');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        done();
+      });
+  });
+  it('should get all posted property adverts of same type', (done) => {
+    request(app)
+      .get('/api/v1/property?type=3 Bedroom')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res).to.have.status('200');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        done();
+      });
+  });
+  it('should return error for unavaible property adverts of same type', (done) => {
+    request(app)
+      .get('/api/v1/property?type=4 Bedroom')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res).to.have.status('404');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('error');
+        done();
+      });
+  });
+});
+
+describe('GET /api/v1/property/:property_id', () => {
+  it('should get a posted property advert', (done) => {
+    request(app)
+      .get('/api/v1/property/1')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res).to.have.status('200');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        done();
+      });
+  });
+  it('should get a posted property advert', (done) => {
+    request(app)
+      .get('/api/v1/property/u')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Id must be a number');
+        done();
+      });
+  });
+  it('should return an error if the property is not found', (done) => {
+    request(app)
+      .get('/api/v1/property/1200')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res).to.have.status('404');
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Advert not found');
+        done();
+      });
+  });
+});
+describe('DELETE /api/v1/property/:property_id', () => {
+  it('should return an error if no token is provided', (done) => {
+    request(app)
+      .delete('/api/v1/property/1')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.be.equal(401);
+        expect(res).to.have.status('401');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        done();
+      });
+  });
+  it('should return delete a property', (done) => {
+    request(app)
+      .delete('/api/v1/property/1')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res).to.have.status('200');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('data');
+        expect(res.body.data).to.be.equal('Advert Successfully deleted');
+        done();
+      });
+  });
+  it('should return error for invalid id type', (done) => {
+    request(app)
+      .delete('/api/v1/property/u')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(400);
+        expect(res).to.have.status('400');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Id must be a number');
+        done();
+      });
+  });
+  it('should return an error is property not found', (done) => {
+    request(app)
+      .delete('/api/v1/property/1200')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res).to.have.status('404');
+        expect(res.body).to.include.key('status');
+        expect(res.body).to.include.key('message');
+        expect(res.body.message).to.be.equal('Advert not found');
+        done();
+      });
+  });
+});
